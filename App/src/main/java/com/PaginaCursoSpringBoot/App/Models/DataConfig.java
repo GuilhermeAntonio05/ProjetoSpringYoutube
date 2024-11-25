da7b3aa6bb011b4ad2dad90ef386269e78b797a1
Guilherme <guilherzer4@gmail.com>
2024-11-24 23:02:01 -0300

    Conexão com banco de dados criada

--- App/src/main/java/com/PaginaCursoSpringBoot/App/Models/DataConfig.java
+++ App/src/main/java/com/PaginaCursoSpringBoot/App/Models/DataConfig.java
@@ -1,15 +1,23 @@
 package com.PaginaCursoSpringBoot.App.Models;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 
 public class DataConfig {
-	Properties props = new Properties();
-	private String url = "jdbc:postegresql://localhost:5432/PaginaCursoSpringBoot";
-	private String user = "postegres";
-	private String password = "1234";
-	
-	
-	Connection conn = DriverManager.getConnection(url, user, password);
+
+	public static void main(String[] args) throws SQLException {
+		Properties props = new Properties();
+		String url = "jdbc:postegresql://localhost:5432/PaginaCursoSpringBoot";
+		props.setProperty("user", "postegres");
+		props.setProperty("password", "1234");
+		props.setProperty("ssl", "true");
+		
+		Connection conn = DriverManager.getConnection(url,props);
+		
+		conn.setAutoCommit(false);
+		
+	}
 }
